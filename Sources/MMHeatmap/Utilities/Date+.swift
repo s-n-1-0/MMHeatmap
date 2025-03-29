53e2d60d996475b356fcdafde6cdfa047244f215
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2025-03-29 16:12:43 +0900

    rename file

--- Sources/MMHeatmap/Utilities/Date+.swift
+++ Sources/MMHeatmap/Utilities/Date+.swift
@@ -13,17 +13,19 @@ extension Date{
         let comps = cal.dateComponents(comps, from: self)
         return cal.date(from: comps)!
     }
+    
     func truncateHms() -> Date{
         truncate([.year, .month, .day])
     }
+    
     func getYmdhms() -> Ymdhms?{
         let cal = Calendar(identifier: .gregorian)
         let comps = cal.dateComponents(
             [Calendar.Component.year, Calendar.Component.month, Calendar.Component.day,
              Calendar.Component.hour, Calendar.Component.minute, Calendar.Component.second],
-             from: self)
+            from: self)
         if let year = comps.year, let month = comps.month, let day = comps.day, let hour = comps.hour, let minute = comps.minute, let second = comps.second{
-         return Ymdhms(year: year, month: month, day: day, hour: hour, minute: minute, second: second)
+            return Ymdhms(year: year, month: month, day: day, hour: hour, minute: minute, second: second)
         }
         return nil
     }
